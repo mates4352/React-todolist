b84b913f63f26ec0ab29b515b041a0076e832e00
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-01-26 21:50:46 +0300

    Add logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,51 +4,55 @@ import {v1} from "uuid";
 
 const App = () => {
 
-   let [tasks, setState] = useState<Array<dataTodolistType>>(
-       [
-          {id: v1(), isDown: true, label: "Html-Css"},
-          {id: v1(), isDown: true, label: "Js"},
-          {id: v1(), isDown: false, label: "ReactJs"},
-       ]
-   );
+  let [tasks, setTasks] = useState<Array<dataTodolistType>>(
+    [
+      {id: v1(), isDown: true, text: "Html-Css"},
+      {id: v1(), isDown: true, text: "Js"},
+      {id: v1(), isDown: false, text: "ReactJs"},
+    ]
+  );
+  let [filter, setFilter] = useState<changeFilterType>("All");
 
-   let [filter, setFilter] = useState<changeFilterType>("All");
+  const changeFilterTasks = (taskId: string) => {
+    setTasks(tasks.map( task => task.id === taskId ? {...task, isDown: !task.isDown} : task))
+  }
 
-   const removeTask = (id: string) => {
-      setState(tasks.filter(item => item.id !== id))
-   }
+  const removeTask = (id: string) => {
+    setTasks(tasks.filter(item => item.id !== id))
+  }
 
-   const changeFilter = (value: changeFilterType) => {
-      setFilter(value)
-   }
+  const changeFilter = (value: changeFilterType) => {
+    setFilter(value)
+  }
 
-   const getTasks = () => {
-      switch (filter) {
-         case "Active":
-            return tasks.filter((task => task.isDown));
+  const getTasks = () => {
+    switch (filter) {
+      case "Active":
+        return tasks.filter((task => task.isDown));
 
-         case "Completed":
-            return tasks.filter((task => !task.isDown));
+      case "Completed":
+        return tasks.filter((task => !task.isDown));
 
-         default:
-            return tasks;
-      }
-   }
-   const tasksForTodoList = getTasks();
+      default:
+        return tasks;
+    }
+  }
+  const tasksForTodoList = getTasks();
 
-   const getValue = (value: string) => {
-      const task = {id: v1(), isDown: true, label: value};
-      setState( [...tasks, task])
-   }
+  const getValue = (value: string) => {
+    const task = {id: v1(), isDown: true, text: value};
+    setTasks([...tasks, task])
+  }
 
-   return (
-       <Todolist
-           tasks={tasksForTodoList}
-           title={"Todolist"}
-           removeTask={removeTask}
-           changeFilter={changeFilter}
-           getValue = {getValue}/>
-   );
+  return (
+    <Todolist
+      tasks={tasksForTodoList}
+      title={"Todolist"}
+      removeTask={removeTask}
+      changeFilter={changeFilter}
+      getValue={getValue}
+      changeFilterTasks={changeFilterTasks}/>
+  );
 }
 
 export default App;
--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -2,94 +2,102 @@ import s from './Todolist.module.scss'
 import React, {ChangeEvent, useState} from "react";
 
 export type dataTodolistType = {
-   id: string
-   label: string
-   isDown: boolean
+  id: string
+  text: string
+  isDown: boolean
 }
 
 type todolistType = {
-   title: string
-   tasks: Array<dataTodolistType>
-   removeTask: (id: string) => void;
-   changeFilter: (value: changeFilterType) => void;
-   getValue: (value: string) => void;
+  title: string
+  tasks: Array<dataTodolistType>
+  removeTask: (id: string) => void;
+  changeFilter: (value: changeFilterType) => void;
+  getValue: (value: string) => void;
+  changeFilterTasks: (taskId: string) => void;
 }
 
 export type changeFilterType = "All" | "Active" | "Completed";
 
 export const Todolist: React.FC<todolistType> = ({
-                                                    title,
-                                                    tasks,
-                                                    removeTask,
-                                                    changeFilter,
-                                                    getValue
+                                                   title,
+                                                   tasks,
+                                                   removeTask,
+                                                   changeFilter,
+                                                   getValue,
+                                                   changeFilterTasks
                                                  }) => {
-   const [inputValue, setInputValue] = useState('');
-   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-      setInputValue(e.currentTarget.value)
-   }
-   const onKeyUpInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === "Enter") {
-         getValue(inputValue)
-         setInputValue('')
-      }
-   }
-   const onClinkButton = () => {
-         getValue(inputValue)
-         setInputValue('')
-   }
 
-   return (
-       <div className={s.todolist}>
-          <h1 className={s.title}>{title}</h1>
+  const [inputValue, setInputValue] = useState('');
+  const onChangeInput = (element: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(element.currentTarget.value)
+  }
+  const onKeyUpInput = (key: React.KeyboardEvent<HTMLInputElement>) => {
+    if (key.key === "Enter" && key.currentTarget.value !== "") {
+      getValue(inputValue)
+      setInputValue('')
+    }
+  }
+  const onClinkButton = () => {
+    getValue(inputValue)
+    setInputValue('')
+  }
 
-          <div className={s.input_wrap}>
-             <input className={s.input} type="text" value={inputValue} onChange={e => {
-                onChangeInput(e)
-             }} onKeyUp={(e) => {
-                onKeyUpInput(e)
-             }}/>
-             <button className={s.button} type={"button"} onClick={() => {
-                onClinkButton()
-             }}>+
-             </button>
-          </div>
+  return (
+    <div className={s.todolist}>
+      <h1 className={s.title}>{title}</h1>
 
-          <ul className={s.list}>
-             {tasks.map((item) =>
-                 <li className={s.item} key={item.id}>
-                    <input type="checkbox" checked={item.isDown}/>
-                    <label>{item.label}</label>
-                    <button className={s.close} onClick={() => {
-                       removeTask(item.id)
-                    }}>X
-                    </button>
-                 </li>
-             )}
-          </ul>
+      <div className={s.input_wrap}>
+        <input
+          className={s.input}
+          type="text"
+          value={inputValue}
+          onChange={element => onChangeInput(element)}
+          onKeyUp={key => onKeyUpInput(key)}/>
 
-          <ul className={s.sublist}>
-             <li className={s.subitem}>
-                <button className={s.subitem_button} onClick={() => {
-                   changeFilter("All")
-                }}>All
-                </button>
-             </li>
+        <button className={s.button}
+                type={"button"}
+                onClick={onClinkButton}>
+          +
+        </button>
+      </div>
 
-             <li>
-                <button className={s.subitem_button} onClick={() => {
-                   changeFilter("Active")
-                }}>Active
-                </button>
-             </li>
+      <ul className={s.list}>
+        {tasks.map((item) =>
+          <li className={s.item} key={item.id}>
+            <input type="checkbox"
+                   id={item.id}
+                   checked={item.isDown}
+                   onClick={() => changeFilterTasks(item.id)}
+                   onKeyUp={key => key.key === "Enter" && changeFilterTasks(item.id)}/>
 
-             <li>
-                <button className={s.subitem_button} onClick={() => {
-                   changeFilter("Completed")
-                }}>Completed
-                </button>
-             </li>
-          </ul>
-       </div>
-   );
+            <label htmlFor={item.id}>{item.text}</label>
+
+            <button className={s.close} onClick={() => removeTask(item.id)}>
+              X
+            </button>
+          </li>
+        )}
+      </ul>
+
+      <ul className={s.sublist}>
+        <li className={s.subitem}>
+          <button className={s.subitem_button} onClick={() => changeFilter("All")}>
+            All
+          </button>
+        </li>
+
+        <li>
+          <button className={s.subitem_button} onClick={() => changeFilter("Active")}>
+            Active
+          </button>
+        </li>
+
+        <li>
+          <button className={s.subitem_button} onClick={() => changeFilter("Completed")}>
+            Completed
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
